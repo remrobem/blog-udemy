363630e60f880fee7cad5f3e16b94ff730ba4740
Rob Martin <rob.martin.5758@gmail.com>
2019-09-03 15:03:08 -0400

    basic get, post delete

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -12,7 +12,7 @@ class FullPost extends Component {
     componentDidUpdate() {
         // ponly get if have id and either no post loaded or loaded post not equal to new selected post
         if (this.props.id) {
-            if ( !this.state.loadedPost || ( this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
+            if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
                 axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
                     .then(response => {
                         this.setState({ loadedPost: response.data })
@@ -20,6 +20,13 @@ class FullPost extends Component {
                     });
             }
         };
+    };
+
+    deletePostHandler = () => {
+        axios.delete('https://jsonplaceholder.typicode.com/posts/' + this.state.loadedPost.id)
+            .then((response) => {
+                console.log(response)
+            });
     };
 
     render() {
@@ -35,7 +42,7 @@ class FullPost extends Component {
                     <h1>{this.state.loadedPost.title}</h1>
                     <p>{this.state.loadedPost.body}</p>
                     <div className="Edit">
-                        <button className="Delete">Delete</button>
+                        <button className="Delete" onClick={this.deletePostHandler}>Delete</button>
                     </div>
                 </div>
 
